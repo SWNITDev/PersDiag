5572bf313c9bb8d6065df3a22e5b55390018ee39
SWNITDev <github@swn.com>
2023-08-11 11:50:34 +0200

    SAML2 entfernt und aus .env variable entfernt
    // Datenbankinfo entfernt
    // Saml2 Dateien entfernt
    // Aus composer saml2 entfernt

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\SAML2ServiceProvider::class,
     ])->toArray(),
 
     /*
